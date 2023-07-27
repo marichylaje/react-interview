3e429ba9817fc5833cd05250e65c2d72585453fd
Mariano Laje Arrigoni <marianoarrigoni@outlook.com>
2023-07-27 19:58:39 +0200

    created main App file, consuming other already created components

---
+++ src/App.tsx
@@ -0,0 +1,87 @@
+import React, { useEffect, useState } from "react"
+import { Button, Grid, ThemeProvider, createTheme } from "@mui/material"
+import CssBaseline from '@mui/material/CssBaseline';
+import Container from '@mui/material/Container';
+import { styled } from '@mui/system';
+
+import globalTheme from "./theme";
+import './App.css'
+
+import {MovingTitle} from "components"
+import {MovingSubtitle} from "components";
+import {SetButtonOptions} from "components";
+import Pages from "pages";
+import { updateContentfulTeorical, updateContentfulQuestionaire, initTContentStore, initTQuestionairetStore } from "store";
+import type { QuestionaireType, TeoricalType } from "store";
+import { useDispatch } from "react-redux";
+
+const StyledDivTitle = styled("div")`
+  margin: 30px 0px;
+`;
+
+const StyledGoBackButton = styled(Button)`
+  position: fixed;
+  top: 10px;
+  right: 10px;
+  background-color: ${globalTheme.palette.grey.light};
+  border: 1px solid ${globalTheme.palette.grey.main};
+  color: ${globalTheme.palette.primary.dark};
+`;
+
+const App: React.FC = () => {
+  const theme = createTheme(globalTheme);
+  const dispatch = useDispatch();
+
+  const titleComp: string = "Welcome to";
+  const title: string = "React Interview Training";
+
+  const [clickedBtnArrayPos, setClickedBtnArrayPos] = useState<number>(-1);
+  const [ teoricalData, setTeoricalData ] = useState<TeoricalType | []>([]);
+  const [ questionaireData, setQuestionaireData ] = useState<QuestionaireType | []>([]);
+
+  useEffect(() => {
+    initTContentStore(setTeoricalData)
+    initTQuestionairetStore(setQuestionaireData)
+  }, []);
+
+  useEffect(() => {
+    teoricalData && dispatch(updateContentfulTeorical(teoricalData))
+  }, [teoricalData]);
+
+  useEffect(() => {
+    questionaireData && dispatch(updateContentfulQuestionaire(questionaireData))
+  }, [questionaireData]);
+
+  const goBack = () => {
+    setClickedBtnArrayPos(-1)
+  }
+  
+  return (
+    <>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+
+        <Container maxWidth="lg">
+          <StyledGoBackButton href={"/#"} onClick={goBack}>Go Back</StyledGoBackButton>
+          <StyledDivTitle>
+            <MovingTitle titleProp={titleComp}/>
+            <MovingSubtitle titleProp={title}/>
+          </StyledDivTitle>
+
+          <Grid container spacing={2}>  
+            {
+              clickedBtnArrayPos >= 0 
+              ?
+                <Pages clickedBtnArrayPos={clickedBtnArrayPos} />
+              :
+                <SetButtonOptions setClickedBtnArrayPos={setClickedBtnArrayPos}/>
+            }
+          </Grid>
+        </Container>
+      </ThemeProvider>
+    </>
+
+  )
+}
+
+export default App;
