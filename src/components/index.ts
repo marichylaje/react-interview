191611332bbed6daa556e4b12a14d2cab36b6509
Mariano Laje Arrigoni <marianoarrigoni@outlook.com>
2023-07-27 20:07:10 +0200

    Feat: easy Export all Components

---
+++ src/components/index.ts
@@ -0,0 +1,21 @@
+import ButtonOptions from "./ButtonOptions";
+import MenuList from "./MenuList";
+import MovingSubtitle from "./MovingSubtitle";
+import MovingTitle from "./MovingTitle";
+import Questionaire from "./Questionaire";
+import QuestionaireResponse from "./QuestionaireResponse";
+import RichText from "./RichText";
+import SetButtonOptions from "./SetButtonOptions";
+import TeoricGrid from "./TeoricGrid";
+
+export { 
+    ButtonOptions,
+    MenuList,
+    MovingSubtitle,
+    MovingTitle,
+    Questionaire,
+    QuestionaireResponse,
+    RichText,
+    SetButtonOptions,
+    TeoricGrid
+}
