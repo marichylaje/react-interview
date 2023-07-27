d5f5b9da774e74703f1ec8cee6929f60db5f0646
Mariano Laje Arrigoni <marianoarrigoni@outlook.com>
2023-07-27 20:06:02 +0200

    Feat: Coding Challenge Page

---
+++ src/pages/CodingChallenges/CodingChallenges.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export type CodingChallengesProps = {
+}
+
+const CodingChallenges: React.FC<CodingChallengesProps> = () => {
+    return (
+        <div>
+        
+        </div>
+    );
+}
+
+export default CodingChallenges;
