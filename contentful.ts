dbb479cf50637d789624f31474072d36aa55e076
Mariano Laje Arrigoni <marianoarrigoni@outlook.com>
2023-08-29 13:18:17 +0200

    fix: properly added contentful connection and absolute path

--- contentful.ts
+++ contentful.ts
@@ -1,13 +1,13 @@
 import { createClient } from 'contentful';
-import dotenv from 'dotenv';
+/*import dotenv from 'dotenv';
 
 // Load environment variables from .env file
-dotenv.config();
+dotenv.config();*/
 
 const client = createClient({
-  space: process.env.CONTENTFUL_SPACE,
-  environment: process.env.CONTENTFUL_ENVIRONMENT,
-  accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+  space: import.meta.env.VITE_CONTENTFUL_SPACE,
+  environment: import.meta.env.VITE_CONTENTFUL_ENVIRONMENT,
+  accessToken: import.meta.env.VITE_CONTENTFUL_ACCESS_TOKEN,
 });
 
 export default client;
