4f3d7b6593a9c0ed74c60d3b6df34de4602d3fe6
Mariano Laje Arrigoni <marianoarrigoni@outlook.com>
2023-08-29 13:14:06 +0200

    fix: define paths for .env credentials

--- vite.config.ts
+++ vite.config.ts
@@ -7,4 +7,9 @@ export default defineConfig({
     react(),
     tsconfigPaths(),
   ],
+  define: {
+    'process.env.VITE_CONTENTFUL_SPACE': process.env.CONTENTFUL_SPACE,
+    'process.env.VITE_CONTENTFUL_ENVIRONMENT': process.env.CONTENTFUL_ENVIRONMENT,
+    'process.env.VITE_CONTENTFUL_ACCESS_TOKEN': process.env.CONTENTFUL_ACCESS_TOKEN,
+  },
 });
