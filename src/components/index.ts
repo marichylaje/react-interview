8d94976a929966a9be2bcda48a0553885dd90177
Mariano Laje Arrigoni <marianoarrigoni@outlook.com>
2023-08-30 15:48:41 +0200

    fix: correct import for questionaire subcomponents

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,7 +3,7 @@ import MenuList from "./MenuList";
 import MovingSubtitle from "./MovingSubtitle";
 import MovingTitle from "./MovingTitle";
 import Questionaire from "./Questionaire";
-import QuestionaireResponse from "./QuestionaireResponse";
+import QuestionaireResponse from "./Questionaire/QuestionaireResponse";
 import RichText from "./RichText";
 import SetButtonOptions from "./SetButtonOptions";
 import TeoricGrid from "./TeoricGrid";
